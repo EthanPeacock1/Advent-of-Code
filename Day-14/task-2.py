bfd42accc019ef447d16a249f74307de189fccf4
Ethan Peacock <116737628+EthanPeacock1@users.noreply.github.com>
2022-12-14 22:14:02 +0000

    Day 14 ☃️

---
+++ Day-14/task-2.py
@@ -0,0 +1,100 @@
+rocks = set()
+
+def readInputs():
+	with open("Day-14/inputs.txt", "r") as file:
+		data = file.read().splitlines()
+	return data
+
+def debug(coords):
+	global rocks
+	smallestX = min([c[0] for c in coords])
+	biggestX = max([c[0] for c in coords]) + 1
+	smallestY = min([c[1] for c in coords])
+	biggestY = max([c[1] for c in coords]) + 1
+
+	output = "\n"
+	for y in range(smallestY, biggestY):
+		for x in range(smallestX, biggestX):
+			currentCoord = (x, y)
+			if currentCoord in rocks:
+				output += "# "
+			else:
+				output += ". "
+		output += "\n"
+
+	print(output)
+
+def createRocks(raw):
+	global rocks
+	for line in raw:
+		coords = []
+		for stringCoord in line.split(" -> "):
+			stringX, stringY = stringCoord.split(",")
+			x, y = int(stringX), int(stringY)
+			coords.append((x, y))
+
+		# For every coord generated from above, compared to previous
+		for coord in range(1, len(coords)):
+			x = coords[coord][0]
+			y = coords[coord][1]
+			px = coords[coord-1][0]
+			py = coords[coord-1][1]
+
+			# If x XOR y is different, generate the gaps
+
+			if y != py and x == px:
+				for ny in range(min(y, py), max(y, py) + 1):
+					rocks.add((x, ny))
+			
+			if x != px and y == py:
+				for nx in range(min(x, px), max(x, px) + 1):
+					rocks.add((nx, y))
+		
+	debug(coords)
+
+def dropSand(biggestY):
+	global rocks
+	currentX = 500
+	currentY = 0
+
+	if (currentX, currentY) in rocks:
+		return (currentX, currentY)
+
+	while currentY <= biggestY:
+		# down
+		if (currentX, currentY + 1) not in rocks:
+			currentY += 1
+			continue
+		# left
+		if (currentX - 1, currentY + 1) not in rocks:
+			currentX -= 1
+			currentY += 1
+			continue
+		# right
+		if (currentX + 1, currentY + 1) not in rocks:
+			currentX += 1
+			currentY += 1
+			continue
+
+		break
+	# We now return the new coords the sand has moved to
+	return (currentX, currentY)
+
+def main():
+	global rocks
+	raw = readInputs()
+	createRocks(raw)
+	biggestY = max([c[1] for c in rocks])
+	total = 0
+
+	while True:
+		x, y = dropSand(biggestY)
+		rocks.add((x, y))
+		total += 1
+		# If the sand has reached the drop point, no more sand can drop
+		if (x, y) == (500, 0):
+			break
+	
+	print("total:", total)
+
+main()
